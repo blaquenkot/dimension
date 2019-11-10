64cb6020e9c54cc340d5baf46f2911c6412568e6
Julio <julio.olvr@gmail.com>
2019-11-10 16:56:53 -0300

    Move left and right

--- Square.gd
+++ Square.gd
@@ -4,6 +4,7 @@ export(NodePath) var initial_rail
 var rail
 var speed = 20
 var leap_distance = speed * 2
+var last_direction
 
 func _ready():
 	rail = get_node(initial_rail)
@@ -12,8 +13,24 @@ func _ready():
 	snap_to_rail(rail)
 
 func _process(delta):
+	if Input.is_action_just_pressed("right"):
+		if last_direction == "right" && !$LeapTimer.is_stopped():
+			leap(Vector3.RIGHT)
+		else:
+			$LeapTimer.start()
+
+	if Input.is_action_just_pressed("left"):
+		if last_direction == "left" && !$LeapTimer.is_stopped():
+			leap(Vector3.LEFT)
+		else:
+			$LeapTimer.start()
+
 	if Input.is_action_pressed("right"):
+		last_direction = "right"
 		move_and_collide(Vector3.RIGHT * speed * delta)
+	elif Input.is_action_pressed("left"):
+		last_direction = "left"
+		move_and_collide(Vector3.LEFT * speed * delta)
 
 	if Input.is_action_just_pressed("up"):
 		var rail_above = rail.get_rail_above_at(translation)
@@ -28,15 +45,10 @@ func _process(delta):
 			snap_to_rail(rail_below)
 			rail = rail_below
 
-	if Input.is_action_just_pressed("right"):
-		if !$LeapTimer.is_stopped():
-			leap()
-		else:
-			$LeapTimer.start()
-
-func leap():
+func leap(direction):
+	$LeapTimer.stop()
 	visible = false
-	move_and_collide(Vector3.RIGHT * leap_distance)
+	move_and_collide(direction * leap_distance)
 	visible = true
 
 func snap_to_rail(rail):
